3ef654d232b9a14c5709193c5f24763e05e15c1a
MiksuMaker <91052332+MiksuMaker@users.noreply.github.com>
2021-10-05 16:14:03 +0300

    Adding Score Text

--- Flappy Clone/Assets/Scripts/GameManager.cs
+++ Flappy Clone/Assets/Scripts/GameManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
@@ -19,4 +20,8 @@ public class GameManager : MonoBehaviour
         Time.timeScale = 0;
     }
 
+    public void Replay()
+    {
+        SceneManager.LoadScene(0);
+    }
 }
