71eb7adb72d39923c148cb6f063bcfe32cb86347
MiksuMaker <91052332+MiksuMaker@users.noreply.github.com>
2021-10-05 16:05:09 +0300

    Functional Game Over

--- Flappy Clone/Assets/Scripts/FlyScript.cs
+++ Flappy Clone/Assets/Scripts/FlyScript.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class FlyScript : MonoBehaviour
 {
-
+    public GameManager gameManager;
     public float velocity = 1;
     private Rigidbody2D rb;
 
@@ -22,5 +22,10 @@ public class FlyScript : MonoBehaviour
         {
             rb.velocity = Vector2.up * velocity;
         }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        gameManager.GameOver();
     }
 }
--- Flappy Clone/Assets/Scripts/GameManager.cs
+++ Flappy Clone/Assets/Scripts/GameManager.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    [SerializeField] public GameObject gameOverCanvas;
+
+
     void Start()
     {
-        
+        Time.timeScale = 1;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void GameOver()
     {
-        
+        gameOverCanvas.SetActive(true);
+        Time.timeScale = 0;
     }
+
 }
