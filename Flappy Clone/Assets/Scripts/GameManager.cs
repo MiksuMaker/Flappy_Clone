33fe15c78f6e2d5b55d8ceb4caea4c7762469e0c
MiksuMaker <91052332+MiksuMaker@users.noreply.github.com>
2021-10-05 15:44:51 +0300

    Revert "Lost the Ability to add UI"
    This reverts commit cec5facbc2a153af971f29c1811c95bc6dae25a7.

--- Flappy Clone/Assets/Scripts/GameManager.cs
+++ Flappy Clone/Assets/Scripts/GameManager.cs
@@ -1,7 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
@@ -17,13 +16,9 @@ public class GameManager : MonoBehaviour
     public void GameOver()
     {
         gameOverCanvas.SetActive(true);
-        Time.timeScale = 0;     // Pys‰ytt‰‰ pelin sis‰isen ajan
+        //Time.timeScale = 0;
     }
 
-    public void Replay()
-    {
-        SceneManager.LoadScene(0);
-    }
 
 
 }
