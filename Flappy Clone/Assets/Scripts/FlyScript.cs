0a0faabd37b6fdc03761ba7db798f28898873227
MiksuMaker <91052332+MiksuMaker@users.noreply.github.com>
2021-10-21 17:03:20 +0300

    Added Sounds

--- Flappy Clone/Assets/Scripts/FlyScript.cs
+++ Flappy Clone/Assets/Scripts/FlyScript.cs
@@ -5,6 +5,11 @@ using UnityEngine;
 public class FlyScript : MonoBehaviour
 {
     public GameManager gameManager;
+    [SerializeField] AudioClip jumpSound;
+    [SerializeField] float jumpVolyme = 1f;
+    [SerializeField] AudioClip deathSound;
+    [SerializeField] float deathVolyme = 1f;
+
     public float velocity = 1;
     private Rigidbody2D rb;
 
@@ -24,6 +29,7 @@ public class FlyScript : MonoBehaviour
         if(Input.GetKeyDown(KeyCode.Space) && stillAlive)
         {
             rb.velocity = Vector2.up * velocity;
+            AudioSource.PlayClipAtPoint(jumpSound, Camera.main.transform.position, jumpVolyme);
         }
     }
 
@@ -31,5 +37,6 @@ public class FlyScript : MonoBehaviour
     {
         stillAlive = false;
         gameManager.GameOver();
+        AudioSource.PlayClipAtPoint(deathSound, Camera.main.transform.position, deathVolyme);
     }
 }
